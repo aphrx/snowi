c42269541f1c21758066ce47eeb2a885b1b46fa1
aphrx <amalnnath20@yahoo.ca>
2020-02-18 00:28:40 +0000

    motor controls

--- test_cam.py
+++ test_cam.py
@@ -1,7 +1,19 @@
 import numpy as np
 import cv2
+import RPi.GPIO as GPIO
 
 cap = cv2.VideoCapture(0)
+
+in1 = 27
+in2 = 22
+in3 = 24
+in4 = 25
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
 
 while(True):
     # Capture frame-by-frame
@@ -10,7 +22,19 @@ while(True):
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.LOW)
         break
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('w'):
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(in3, GPIO.HIGH)
+        GPIO.output(in4, GPIO.LOW)
+
 
 # When everything done, release the capture
 cap.release()
